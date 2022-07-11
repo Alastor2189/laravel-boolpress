a2872beed395cb1a4c318d0c1723d0fe65323300
Alastor2189 <g.lucafarci89@gmail.com>
2022-07-12 00:42:55 +0200

    implementato il tag nei file index-show-create-edit della cartella admin, creazione della tabella tag e del ponte annesso

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -19,4 +19,5 @@
             {{-- Card --}}
         @endforeach
     </div>
+    {{ $posts->links() }}
 @endsection
--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -4,11 +4,27 @@
     <h1> {{ $post->title }} </h1>
     <p>Slug: {{ $post->slug }}</p>
     <p>Categoria: {{ $category ? $category->name : 'nessuna categoria' }}</p>
+    <p><strong>Tags: </strong>
+
+        @forelse ($post->tags as $tag)
+            {{ $tag->name }}{{ $loop->last ? '' : ', ' }}
+        @empty
+            nessuno
+        @endforelse
+    </p>
     <p>{{ $post->content }}</p>
 
     <div class="d-flex justify-content-between" style="max-width: 150px; min-width: 120px">
-        <a class="btn btn-primary" href="{{ route('admin.posts.edit', ['post' => $post->id]) }}">Modifica
-        </a>
+        <div class="d-flex">
+            <a class="btn btn-primary" href="{{ route('admin.posts.edit', ['post' => $post->id]) }}">Modifica</a>
+    
+            <form action="{{ route('admin.posts.destroy', ['post' => $post->id]) }}" method="post">
+                @csrf
+                @method('DELETE')
+    
+                <button class="btn btn-danger" type="submit">Cancella</button>
+            </form>
+        </div>
         <form action="{{ route('admin.posts.destroy', ['post' => $post->id]) }}" method="post">
             @csrf
             @method('delete')
