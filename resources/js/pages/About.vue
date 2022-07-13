59f2d1c2776d9fc14e1f05a097aafd84d49450c6
Alastor2189 <g.lucafarci89@gmail.com>
2022-07-13 17:56:42 +0200

    implementazione pagina blog

---
+++ resources/js/pages/About.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container">
+        <h1>About</h1>
+        <a href="https://www.youtube.com/watch?v=XIBXXtGMOVM&ab_channel=MetalLyricVideos">MJ</a>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "About",
+};
+</script>
+
+<style></style>
---
+++ resources/js/pages/Blog.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+        <h2>Blog</h2>
+        <WorkInProgress />
+    </div>
+</template>
+
+<script>
+import WorkInProgress from "../components/WorkInProgress.vue";
+export default {
+    name: "Blog",
+    components: {
+        WorkInProgress,
+    },
+};
+</script>
+
+<style></style>
