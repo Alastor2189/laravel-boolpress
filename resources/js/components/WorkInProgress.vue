5914fe143f64e5615afa16adccbe28d1d2e40523
Alastor2189 <g.lucafarci89@gmail.com>
2022-07-13 17:40:24 +0200

    installazione vue router e aggiunta pagina homepage

--- resources/js/components/WorkInProgress.vue
+++ resources/js/components/WorkInProgress.vue
@@ -6,14 +6,14 @@
             <div class="row row-cols-2">
                 <div v-for="item in posts" :key="item.id" class="col">
                     <div class="card mb-4">
-                        <div class="contain-card">
-                            <h5 class="title-card">{{item.title}}</h5>
+                        <div class="card-body">
+                            <h5 class="card-title">{{item.title}}</h5>
                             <p>{{ item.content }}</p>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+    </div>
   </div>
 </template>
 
---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home-container">
+    <h1>
+        Homepage
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"Homepage",
+};
+</script>
+
+<style>
+
+</style>
